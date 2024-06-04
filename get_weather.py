8f90b44bfbd855d4da147a9a7a04483033563ca7
inf <manskiy2k@gmail.com>
2024-06-04 19:07:16 +0300

    payload теперь словарь

--- get_weather.py
+++ get_weather.py
@@ -3,8 +3,9 @@ import requests
 
 def get_weather(location):
     url = 'https://wttr.in'
-    payload = "TMqn&lang=ru"
+    payload = {'TMqn': '', 'lang': 'ru'}
     response = requests.get(f"{url}/{location}", params=payload)
+    print(response.url)
     response.raise_for_status()
     return response.text
 
